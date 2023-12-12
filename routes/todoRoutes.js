3b1e6ad3db344b87e026a5cd494fca6c8068a968
AwaisYounas72 <151017136+AwaisYounas72@users.noreply.github.com>
2023-12-12 18:18:27 +0500

    Update todoRoutes.js

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { AddTodo, DeleteTodo } from "../controller/todoController.js";
+import { AddTodo, DeleteTodo,GetTodo } from "../controller/todoController.js";
 
 const router = express.Router();
 
 router.post("/addTodo",AddTodo)
 router.delete("/deleteTodo/:id",DeleteTodo)
+router.get("/get-todos",GetTodo)
 
 export default router;
