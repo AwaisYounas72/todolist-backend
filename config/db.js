a56910fff1c4cbec95e472544e37535baf3cd3f2
AwaisYounas72 <151017136+AwaisYounas72@users.noreply.github.com>
2023-12-12 18:23:35 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.DATABASE);
+    const connection = await mongoose.connect("mongodb+srv://awais:19ntu1151@todo-app.nsjx3hg.mongodb.net/");
     console.log(`Connected to MongoDB at ${connection.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
