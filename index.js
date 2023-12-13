ce701557b39f4b54fd843e373a4d37ce8585d644
AwaisYounas72 <151017136+AwaisYounas72@users.noreply.github.com>
2023-12-13 12:00:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.use(cors({
     methods:["POST","GET","DELETE","PUT"],
     credentials:true
 }))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // Database Connection
 ConnectDB()
 
